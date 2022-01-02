68723538e50da74be1c68258b50a3517b1787a3e
l bates childs <labchilds@gmail.com>
2022-01-02 18:51:08 -0500

    get and print current weather functions added

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,41 +6,152 @@ var cityNameEl = $('#city-name');
 var currentWeatherEl = $('#current-weather');
 var forecastEl = $('#5-day-container');
 var recentSearchesEl = $('#search-history');
+var resultsEl = $('#results');
 
-// $(searchFormEl).on('click', '#search-btn', function() { console.log(cityInput.val().trim()) });
+// convert city name to lat & lon
+function cityAsLatLon(city) {
+    var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
 
-// function getWeatherData(city){
-//     fetch(apiUrl).then((res) => {
-//         console.log('success!', city, res);
-//     });
-// }
+    // call api for get lat & lon of city
+    fetch(apiUrl).then(res => {
+        if (res.ok) {
+            res.json()
+                // pass lat & lon from obj to get weather func
+                .then(function (data) {
+                    console.log(data);
+                    console.log(data[0].lon, data[0].lat);
+                    console.log();
+                    let lat = data[0].lat;
+                    let lon = data[0].lon;
+                    let cityName = data[0].local_names.en;
+                    getWeatherData(lat, lon, cityName);
+                })
+        } else {
+            alert("We didn't recognize that city. Please try again.");
+        }
+    }).catch(error => {
+        alert("Unable to connect to the server");
+    })
+};
+
+// cityAsLatLon('houston');
 
 // get weather data
-// create api url based on user data
-// call api – fetch()
-    // if response, then pass relevant data to print function save city name to storage
-    // else throw error message
-// catch error
+function getWeatherData(lat, lon, city) {
+    // create api url based on user data
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+        lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=' + apiKey;
+
+    // call api – fetch()    
+    fetch(apiUrl)
+        .then(res => {
+            // if response, then pass relevant data to print function save city name to storage
+            if (res.ok) {
+                res.json().then(data => {
+                    console.log(data);
+                    console.log(city);
+                    let currentWeather = data.current;
+                    let fiveDayWeather = data.daily;
+                    console.log(currentWeather, fiveDayWeather);
+                    printCurrentWeather(currentWeather, city);
+                    printFutureWeather(fiveDayWeather, city);
+                })
+            }
+            // else throw error message
+            else {
+                alert("We didn't recognize that city. Please try again.");
+
+            }
+            // catch error
+        }).catch(error => console.log(error));
+}
 
 // print weather data
-// get city name from data obj
+function printCurrentWeather(currentWeather, city) {
+    
+    // append heading to page
+    // create today's date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+
     // $(h2 for city-name).textContent( city and date from data obj )
-    // add span for obj in textContent
+    cityNameEl.prepend(city + ' ' + today);
+
+    // add weather icon to span
+    let iconUrl = 'https://openweathermap.org/img/w/' + currentWeather.weather[0].icon + '.png';
+    $('#weather-icon').attr('src', iconUrl);
+
+    let temp = document.createElement('p');
+    let wind = document.createElement('p');
+    let humidity = document.createElement('p');
+    let uv = document.createElement('p');
+    let uvColorCode = document.createElement('span');
+
+    temp.textContent = 'Temp: ' + currentWeather.temp + '°F';
+    wind.textContent = 'Wind: ' + currentWeather.wind_speed + ' MPH';
+    humidity.textContent = 'Humidity: ' + currentWeather.humidity + '%';
+
+    uvColorCode.textContent = currentWeather.uvi;
+    uv.textContent = 'UV Index: ';
+    uv.append(uvColorCode);
+
+    // uv index with background color
+    let uvIndex = currentWeather.uvi;
+    if (uvIndex < 3) {
+        uvColorCode.classList = 'btn bg-success bg-gradient text-white';
+    }
+    if (uvIndex >= 3 && uvIndex < 6) {
+        uvColorCode.classList = 'btn bg-warning bg-gradient text-white';
+    }
+    if (uvIndex >= 6 && uvIndex < 8) {
+        uvColorCode.classList = 'btn bg-orange bg-gradient text-white';
+    }
+    if (uvIndex >= 8) {
+        uvColorCode.classList = 'btn bg-danger bg-gradient text-white';
+    }
+    // print weather data to current forecast div
+    currentWeatherEl.addClass('border border-dark');
+    currentWeatherEl.append(temp);
+    currentWeatherEl.append(wind);
+    currentWeatherEl.append(humidity);
+    currentWeatherEl.append(uv);
+}
+
+function printFutureWeather(forecast, city) {
+    // print data to five day forecast div
+    forecastEl.html('<h3>5-Day Forecast:</h3>');
+    console.log(forecast);
+    // loop through 5 day to create cards
+}
 // get weather details from data obj
-    // key is bold, val is normal
-    // append to page in current-weather 
-// loop through 5 day to create cards
-    // append to page in 5-day-container
+// key is bold, val is normal
+// append to page in current-weather
+
+// append to page in 5-day-container
 // print button with city name to search-history and save in stored arr
 
 // start the search (run in event listener)
-// get the user search term (city)
-// pass into get weather data func
+function handleSearchClick(e) {
+    if (e.target.matches('#search-btn')) {
+        e.preventDefault();
+        console.log('you clicked!');
+        // get the user search term (city)
+        let city = cityInput.val().trim();
+        // pass to city coords func
+        cityAsLatLon(city);
+    }
+}
+
+
 
 // store city names
 // get city names arr from storage on load to print
-    // if city names arr is empty, now it's new search term in arr
-    // else .push new term
+// if city names arr is empty, now it's new search term in arr
+// else .push new term
 // save to storage
 
 /* LISTENERS */
@@ -48,4 +159,5 @@ var recentSearchesEl = $('#search-history');
 // if local storage, push to task arr
 
 // listen for button click/new search
+$(searchFormEl).on('click', '#search-btn', handleSearchClick);
 // listen for recent search button click
